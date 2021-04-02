6506d1ba8febc3bbc396892b6e1a420053acf76d
clediano estefenon <cledianoestefenon@gmail.com>
2021-04-02 13:03:27 -0300

    load project

---
+++ src/main/java/com/devdojo/springbootessentials/SpringbootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.devdojo.springbootessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEssentialsApplication.class, args);
+	}
+
+}
